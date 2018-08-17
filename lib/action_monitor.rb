7859a843f1c991f61ef65267744c1f87c671b5b3
stephen.wang <stephen.wang@dji.com>
2018-08-17 17:05:12 +0800

    add log resource id

--- lib/action_monitor.rb
+++ lib/action_monitor.rb
@@ -18,7 +18,7 @@ module ActionMonitor
           columns = action.values.flatten
           after_save do
             columns.each do |column|
-              track_action("update", column: column.to_s, old_value: try("#{column}_was"), new_value: try(column))
+              track_action_update(column.to_s, try("#{column}_was"), try(column))
             end
           end
         end
@@ -28,29 +28,41 @@ module ActionMonitor
     end
   end
 
-  def track_action(action, options)
-    app_name = Settings.dji_track_logger.app_name || Rails.application.class.to_s
-    meta_data = { action: action }
+  def track_action(action)
+    log_action action: action
+  end
 
-    if action == 'update'
-      meta_data = meta_data.merge(detail: options[:column], status: options[:new_value], old_status: options[:old_value])
-    end
+  def track_action_update(column_name, old_value, new_value)
+    return nil if new_value == old_value
+    meta_data = { 
+      action: 'update',
+      content: column_name,
+      status: new_value,
+      old_status: old_value
+    }
 
-    track_logger.tagged(action) do
-      track_logger.info(
-        key: "#{app_name}_#{self.class}",
-        client_id: track_client_id,
-        meta_data: meta_data
-      )
-    end
-  rescue StandardError => e
-    puts e.inspect
+    log_action meta_data
+  end
+
+  def tracked_app_name
+    @app_name ||= Settings.dji_track_logger.app_name || Rails.application.class.to_s
   end
 
   def track_client_id
     return @client_id if @client_id
     current_user_identfier = Settings.dji_track_logger.current_user_identfier
     @client_id = current_user_identfier ? eval(current_user_identfier) : try(:current_user).try(:id)
+  end
+
+  def log_action meta_data
+    track_logger.tagged(action_name) do
+      track_logger.info(
+        key: "#{tracked_app_name}_#{self.class}",
+        client_id: track_client_id,
+        meta_data: meta_data,
+        resource_id: try(:monitor_resource_id) || try(:id)
+      )
+    end
   end
 
   def track_logger
@@ -69,8 +81,4 @@ module ActionMonitor
       )
     end
   end
-end
-
-ActiveSupport.on_load :active_record do
-  include ActionMonitor
 end
